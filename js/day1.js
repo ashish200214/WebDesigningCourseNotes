77bf67382129cc5f1d7a2d9bdbea31b73e60f687
ashish200214 <gawaliashish2002@gmail.com>
2025-07-03 14:29:18 +0530

    js-day-1

---
+++ js/day1.js
@@ -0,0 +1,69 @@
+// 
+/**
+ * JS is used to functionality to the webpage
+ * 
+ *   ##  How to link the JS file (see day1.htm):
+ *          1. Internal 
+ *          2. External
+ *   ## Different output method(print)
+ *      1. console.log()
+ *      2. confirm()
+ *      3. alert()
+ *      4. document.write()
+ *      5. document.writeln()
+ * 
+ *   ## input method
+ *      1. prompt()
+ * 
+ *   ## Initialization or declaration
+ *  token -> combination of keyword, identifier and value.It is smallest unit of programming.
+ *          1. var
+ *          2. let
+ *          3. const
+ * 
+ *  ## Variable :
+ *      1. Name of storage area or memory location.
+ *      2. Variable name should be unique.
+ *      3. variable name not start the digit, but we can use after one digit. (0-9)
+ *      4. variable should start with letter or the $ or _.
+ *      5. Whitespaces is not allowed in the variable name.
+ *      6. predefined keyword or keyword cannot  use as variable name. ex. let, var, const, while, for, etc.
+ *      
+ *  ## Datatype :
+ *     These concept is used for specify the type of the data.
+ *      There are two types of data types :
+ *          1. primitive : 
+ *              1. Primitve data type is immutable.
+ *              2. We cannot modify after creation. 
+ *              3. In primitive we can store only one value.
+ *              4. Ex. string, number, boolean, undefined, null
+ *          2. non-primitive : 
+ *              1. It is mutable.
+ *              2. We can modify after creation.
+ *              3. In non-primitive we can store multiple value.
+ *              4. Ex. function, array, object, data, math.
+ *      
+ *        
+ *      
+ */
+
+// console.log("Hello Ashish");
+// confirm("Submit here")
+// alert("full storage")
+// document.writeln("Hello i am user also")
+// document.write("Hello Ashish")
+
+// name=prompt("Enter your name")
+// console.log(name)
+
+// num1= Number(prompt("Enter 1st no"))
+// num2=Number (prompt("Enter 2nd no"))
+
+// addition = num1+num2
+// alert("Addition is "+addition)
+
+let numbers = 10
+let str = "10"
+console.log(typeof numbers)
+console.log(typeof (numbers+str))
+
