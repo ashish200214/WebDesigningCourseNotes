0b88c720b763b2a9ad0284c1fc4a5097c43b0f5c
ashish200214 <gawaliashish2002@gmail.com>
2025-07-11 16:33:52 +0530

    jquery_day1

---
+++ HTML/event.js
@@ -0,0 +1,21 @@
+function down(){
+    document.body.style.backgroundColor="red"
+    document.body.style.height="90vh"
+}
+
+
+function up(){
+    document.body.style.backgroundColor="white"
+    document.body.style.height="90vh"
+}
+
+function mouseOver(){
+    document.body.style.backgroundColor="grey"
+    document.body.style.height="90vh"
+}
+
+
+function mouseOut(){
+    document.body.style.backgroundColor="white"
+    document.body.style.height="90vh"
+}
---
+++ jquery/day1.js
@@ -0,0 +1,40 @@
+/*
+    There is collection of predefined functions : 
+    
+*/
+// $(document).ready(()=>{
+//     $("#close").click(()=>{
+//         $("p").hide()
+
+//     });
+//     $("#open").click(()=>{
+//         $("p").show()
+//     })
+// })
+
+
+
+// $(document).ready(()=>{
+//     $("#toggle").click(()=>{
+//         $("p").toggle()
+//     })
+// })
+
+// $(document).ready(
+// function apply(){
+// $("slide").click(()=>{
+//     $("p").slideDown()
+// })
+// })
+
+$(document).ready(()=>{
+    $("#btn1").click(()=>{
+        $("p").html("Gawali")
+    })
+
+    //empty() method
+    $("#btn2").click(()=>{
+        $("div").empty()
+    })
+})
+
